e7b31d178318287262f8a80203e0715ec8b56eef
zx <1243236087@qq.com>
2018-02-22 00:06:45 +0800

    init

---
+++ src/interfaces/IPlugin.ts
@@ -0,0 +1,22 @@
+declare interface IPlugin {
+    /**
+     * Position insert to editor
+     */
+    position: string
+    /**
+     * React class
+     */
+    class: any
+    /**
+     * Plugin's action
+     */
+    actions?: {
+        [name: string]: any
+    }
+    /**
+     * Plugin's store
+     */
+    stores?: {
+        [name: string]: any
+    }
+}
